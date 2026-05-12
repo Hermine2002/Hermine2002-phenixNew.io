e3f74886c329fbb986cc04ed55620412faab8618
Hermine2002 <herminesevumyan850@gmail.com>
2026-05-12 13:27:14 +0400

    Add files via upload

---
+++ app/about/page.jsx
@@ -0,0 +1,9 @@
+
+export default function AboutPage() {
+  return (
+    <div>
+      <h1>About Phenix</h1>
+   
+    </div>
+  );
+}
---
+++ app/coin/page.jsx
@@ -0,0 +1,19 @@
+
+export default function CoinPage() {
+  return (
+    <div className="container">
+      <div className="hero">
+        <div className="hero-content">
+          <video className="video" src="https://www.pexels.com/download/video/13037032/" autoPlay loop muted />
+
+          <h1 className="hero-title">PhenixCoin</h1>
+          <p className="hero-subtitle">Coin is a cryptocurrency</p>
+        </div>
+      </div>
+        <div className="coin-card">
+        <h2>Coin</h2>
+        <p>Coin is a cryptocurrency</p>
+      </div>    
+    </div>
+  );
+}
